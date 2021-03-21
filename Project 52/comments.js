fbb44030291b5d709d1d3a2bab64c99a9dc2cefb
ASinha <69646570+anchalgithub@users.noreply.github.com>
2021-03-21 18:36:39 -0400

    Add files via upload

---
+++ Project 52/comments.js
@@ -0,0 +1,43 @@
+//comments on the comment box.
+
+
+//<div class="info">
+//    <label for="example">Chat With Us!
+  //  </label>
+    //<input id="example" type="text"
+      //     name="Ntext" size="40">
+   // <input id="sent" type="submit"  
+     //      value="Send"> 
+//</div>
+  //<p id="para">      
+  //</p>
+//</body>
+//<script>
+  //window.onclick = function(e)
+
+//  (window. onclick) The onclick JavaScript event occurs when the user clicks on an element. It runs a specified line of code when you click a HTML object that has the onclick attribute
+
+  //{   var id =  e.target.id; 
+  //making sure it takes the id from the input part.
+  
+  // if (id === 'sent')  
+  // { var txt = document.getElementById('example').value 
+  //the text should allow itself to write on the comment box.
+  
+    // $( "#para" ).empty().append( txt );
+    //used for emptying the comment box when done/refreshed.
+
+   //}
+  //}
+//</script>
+
+
+
+
+
+
+
+
+
+
+
